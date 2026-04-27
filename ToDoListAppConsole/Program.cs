405b1a942d9398f465992c6ab99e3864e1ccce50
ArifDev-coder <achadzainul67@gmail.com>
2026-04-27 16:45:55 +0700

    Implement basic structure for ToDo List application

--- ToDoListAppConsole/Program.cs
+++ ToDoListAppConsole/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+
+class Program
+{
+    private List<string> UserToDo { get; set; } 
+    private List<string> UserToDo2;
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Tambah Tugas Baru.");
+            Console.WriteLine("2. Hapus Tugas");
+            Console.WriteLine("3. Melihat Semua Tugas");
+            Console.WriteLine("Pilih opsi [1, 2, 3] atau keluar [q]");
+
+            if (int.TryParse(Console.ReadLine(), out int usrOpt))
+            {
+                switch (usrOpt)
+                {
+                    case 1:
+                        Console.WriteLine("Masukkan Tugas Baru");
+                        string newUserToDo = Console.ReadLine;
+
+                }
+            }
+        }
+    }
+}
